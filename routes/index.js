dff854894249aa9de57565ccd081172307f4e4c8
Valter Lima Mesquita <limavalter@gmail.com>
2016-02-23 14:52:33 -0300

    Returning a json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer')
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage, limits: {fileSize:1024*1024} })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/file', upload.single('file-input'), function(req, res, next) {
+  if (req.file){
+    res.json({
+      "size": req.file.size
+    })  
+  }
+  else{
+    res.send(''); 
+  }
+})
 
 module.exports = router;
